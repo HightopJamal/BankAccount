497cb7da895c722562cfc5401b4796c706202cbb
HightopJamal <amirjdaniel@gmail.com>
2017-03-29 16:24:24 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DanielLibrary;
-
-namespace Daniel_assignment6
+using DanielSon;
+using System.Windows.Forms;
+using System.Drawing;
+namespace atmProject
 {
     class Program
-    { 
-        public static int[] PersonData()
-        {
-           DanielLibrary.Name n1 = new Name("Amir", "Daniel");
-            DanielLibrary.Address a1 = new Address("TN", "Nashville", "Joe", "30565");
-
-            DanielLibrary.Name n2 = new Name("Joe", "Smoe");
-            DanielLibrary.Address a2 = new Address("TN", "Nashville", "Joe", "30565");
-
-            DanielLibrary.Person p1 = new Person("20", n1, a1);
-            DanielLibrary.Person p2 = new Person("44", n2, a2);
-            DanielLibrary.Person[] p = new Person[2];
-
-            p[0] = p1;
-            p[1] = p2;
-
-            return p;
-
-          
-        }
-
+    {
         static void Main(string[] args)
         {
-
-            PersonData();
+            Application.Run(new AtmScreen());
         }
-
     }
 }
